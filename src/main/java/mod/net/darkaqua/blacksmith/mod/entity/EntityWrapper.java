40de5ce87d5254ffb98cc8c45c45db48a6a17129
cout970 <thecout970@gmail.com>
2016-01-12 03:21:33 +0100

    fixed compilation erros, derps and more derps
    Signed-off-by: cout970 <thecout970@gmail.com>

--- src/main/java/mod/net/darkaqua/blacksmith/mod/entity/EntityWrapper.java
+++ src/main/java/mod/net/darkaqua/blacksmith/mod/entity/EntityWrapper.java
@@ -1,7 +1,15 @@
 package net.darkaqua.blacksmith.mod.entity;
 
 import net.darkaqua.blacksmith.api.entity.IEntity;
+import net.darkaqua.blacksmith.api.storage.IDataCompound;
+import net.darkaqua.blacksmith.api.util.Cube;
+import net.darkaqua.blacksmith.api.util.EntityRotation;
+import net.darkaqua.blacksmith.api.util.Vect2d;
+import net.darkaqua.blacksmith.api.util.Vect3d;
+import net.darkaqua.blacksmith.api.world.IWorld;
 import net.minecraft.entity.Entity;
+
+import java.util.UUID;
 
 /**
  * Created by cout970 on 08/11/2015.
@@ -16,5 +24,112 @@ public class EntityWrapper implements IEntity{
 
     public Entity getEntity(){
         return entity;
+    }
+
+    //TODO
+
+    @Override
+    public IWorld getWorld() {
+        return null;
+    }
+
+    @Override
+    public void setWorld(IWorld w) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public Vect3d getLookVector() {
+        return null;
+    }
+
+    @Override
+    public Vect3d getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Vect3d pos) {
+
+    }
+
+    @Override
+    public Vect3d getMotion() {
+        return null;
+    }
+
+    @Override
+    public void setMotion(Vect3d mot) {
+
+    }
+
+    @Override
+    public void moveEntity(Vect3d mot) {
+
+    }
+
+    @Override
+    public EntityRotation getEntityRotation() {
+        return null;
+    }
+
+    @Override
+    public void setEntityRotation(EntityRotation rot) {
+
+    }
+
+    @Override
+    public Cube getEntityBoundingBox() {
+        return null;
+    }
+
+    @Override
+    public Cube getEntityCollisionBox() {
+        return null;
+    }
+
+    @Override
+    public Vect2d getEntitySize() {
+        return null;
+    }
+
+    @Override
+    public boolean isDead() {
+        return false;
+    }
+
+    @Override
+    public void setDead() {
+
+    }
+
+    @Override
+    public UUID getEntityUUID() {
+        return null;
+    }
+
+    @Override
+    public void load(IDataCompound data) {
+
+    }
+
+    @Override
+    public void save(IDataCompound data) {
+
+    }
+
+    @Override
+    public IEntity getRiddingEntity() {
+        return null;
+    }
+
+    @Override
+    public IEntity getRiddenByEntity() {
+        return null;
     }
 }
